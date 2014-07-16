bee196737740bda07cade2b24906341f81b2433b
Dilpreet Singh <dilpreet@vinsol.com>
2014-07-16 19:28:32 +0530

    Solving the Exercise using Object Oriented concepts

--- JavaScript_Dom_Ex_1/checkbox_exercise.js
+++ JavaScript_Dom_Ex_1/checkbox_exercise.js
@@ -1,25 +1,51 @@
-var checkboxNames = document.getElementsByClassName('chkbox'),
-    noneId = document.getElementById("selectNone"),
-    selectAllId = document.getElementById("selectAll");
-function selectAll() {
-  //toggling none check box value to false
-  noneId.checked = false;
-  for(var i in checkboxNames) {
-    checkboxNames[i].checked = selectAllId.checked;
+function CheckboxManipulator() {
+  this.init();
   }
+CheckboxManipulator.prototype = {
+  init : function() {
+  this.selectNoneChk = document.getElementById("selectNone");
+  this.selectAllChk = document.getElementById("selectAll");
+  this.checkboxNames = document.getElementsByClassName("chkbox");
+  },
+  toggle : function() {
+    this.selectAllChk.checked = false;
+    this.selectNoneChk.checked = false;
+  },
+  selectAll : function() {
+    //toggling none check box value to false
+    this.selectNoneChk.checked = false;
+    this.traverseCheckbox(true);
+  },
+  selectNone : function() {
+    //toggling selectAll check box value to false
+    this.selectAllChk.checked = false;
+    this.traverseCheckbox(false);
+  },
+  traverseCheckbox : function(value) {
+    for(var i in this.checkboxNames) {
+      this.checkboxNames[i].checked = value;
+    } 
+  },
+  bindEvents: function(){
+    var _this = this; 
+    this.selectAllChk.addEventListener("click",function(){
+      _this.selectAll();
+      });
+    this.selectNoneChk.addEventListener("click",function(){
+      _this.selectNone();
+      });
+    for(var i in this.checkboxNames) {
+    if (this.checkboxNames[i].type == "checkbox") {
+      var manipulateCheckbox = this.checkboxNames[i];
+      manipulateCheckbox.addEventListener("click",function(){
+        _this.toggle();
+        });  
+      }
+    }
+  }
+}      
+function createCheckboxManipulator() {
+  var manipulator = new CheckboxManipulator();
+  manipulator.bindEvents();
 }
-function selectNone() {
-  //toggling selectAll check box value to false
-  selectAllId.checked = false;
-  for(var i in checkboxNames) {
-    checkboxNames[i].checked = false;
-  }
-}
-function toggle(sourceCheckbox) {
-  if((sourceCheckbox.checked == false) && (selectAllId.checked == true)) {
-    selectAllId.checked = false;
-  }
-  if((sourceCheckbox.checked == true) && (noneId.checked == true)){
-    noneId.checked = false;
-  }
-}
+window.onload = createCheckboxManipulator();
--- JavaScript_Dom_Ex_2/checkbox_3.js
+++ JavaScript_Dom_Ex_2/checkbox_3.js
@@ -1,28 +1,56 @@
-var checkboxNames = document.getElementsByClassName('chkbox'),
-    noneId = document.getElementById("selectNone"),
-    count = 0;    
-function selectNone() {
-  for(var i in checkboxNames) {
-    checkboxNames[i].checked = false;
+function CheckboxManipulator() {
+  this.init();
   }
-  count = 0;
-}
-function toggle(sourceCheckbox) { 
-  var countNum = countNumber();
-  if(countNum > 3) {
-    alert("Only 3 can be selected");
-    sourceCheckbox.checked = false;
-  }
-  else if((sourceCheckbox.checked == true) && (noneId.checked == true)) {
-    noneId.checked = false;
-  }
-} 
-function countNumber() {
-  count = 0 ;
-  for(var i in checkboxNames) {
-    if(checkboxNames[i].checked == true) {
-      count++;
+CheckboxManipulator.prototype = {
+  init : function() {
+  this.selectNoneChk = document.getElementById("selectNone");
+  this.checkboxNames = document.getElementsByClassName("chkbox");
+  this.count = 0;
+  },
+  toggle : function(triggeredCheckbox) {
+    this.selectNoneChk.checked = false;
+    this.countNum(triggeredCheckbox);
+  },
+  countNum : function(triggeredCheckbox) {
+    this.count = 0;
+    this.values = "";
+    for(var i in this.checkboxNames) {
+      if(this.checkboxNames[i].checked) {
+        this.count++;
+        if(this.count > 3) {
+          triggeredCheckbox.checked = false;
+          alert("Only 3 can be Selected"+ "\n" + "You Have Already Selected " + this.values);
+          break;
+        }
+        else {
+          this.values += " " + this.checkboxNames[i].value;
+        }
+      }
+    }
+  },
+  selectNone : function() {
+    for(var i in this.checkboxNames) {
+      this.checkboxNames[i].checked = false;
+    }
+    this.count = 0; 
+  },
+  bindEvents: function(){
+    var _this = this; 
+    this.selectNoneChk.addEventListener("click",function(){
+      _this.selectNone();
+      });
+    for(var i in this.checkboxNames) {
+    if (this.checkboxNames[i].type == "checkbox") {
+      var manipulateCheckbox = this.checkboxNames[i];
+      manipulateCheckbox.addEventListener("click",function(){
+        _this.toggle(this);
+        });  
+      }
     }
   }
-  return count;
+}      
+function createCheckboxManipulator() {
+  var manipulator = new CheckboxManipulator();
+  manipulator.bindEvents();
 }
+window.onload = createCheckboxManipulator();
